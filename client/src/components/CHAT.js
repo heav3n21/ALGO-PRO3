ddb9516de1ee8c9bbc2872690141bde11c019dfe
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-19 01:39:34 -0700

    Update CHAT.js
    update to include javascript keyword, function, number and string recognition

--- client/src/components/CHAT.js
+++ client/src/components/CHAT.js
@@ -3,9 +3,9 @@ import CodeMirror from '@uiw/react-codemirror';
 import { createTheme } from '@uiw/codemirror-themes';
 import { javascript } from '@codemirror/lang-javascript';
 import { tags as t } from '@lezer/highlight';
+import '../css/monokai.css';
 import NavBar from './Navbar'
 import AiQuestion from './AIQuestion'
-
 
 
 const myTheme = createTheme({
@@ -28,12 +28,15 @@ const myTheme = createTheme({
       { tag: t.typeName, color: '#194a7b' },
       { tag: t.tagName, color: '#008a02' },
       { tag: t.variableName, color: '#1a00db' },
+      {tag: t.number, color: '#56F212'},
+      {tag: t.string, color: '#C85D31'},
+      {tag: t.keyword, color: '#324FE0'},
+      {tag: t.className, color: '#48AAF7'},
+      
+     
     ],
   });
   
-
-
-
 export default function Chat(){
   const [code, setCode] = useState("console.log('Type your solution here');");
   const handleCodeChange = (value, viewUpdate) => {
@@ -46,17 +49,15 @@ return(
 <>
 <NavBar code={code} updateCodeMirrorValue={updateCodeMirrorValue}/>
 
-<CodeMirror
+<CodeMirror style={{width: '100%'}}
       value={code}
       height="500px"
       theme={myTheme}
+      //theme="monokai"
       extensions={[javascript({ jsx: true })]}
       onChange={handleCodeChange}
     />
-
 </>
-
-
 
 );
 }
