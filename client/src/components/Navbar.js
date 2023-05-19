73d24a709a642456aa12da90adeeb4b20e09bb7d
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-19 02:23:39 -0700

    Update Navbar.js
    update to add comments and clean up code

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -12,90 +12,80 @@ const NavBar = ({code, updateCodeMirrorValue}) => {
   const [showDropdownLevel, setShowDropdownLevel] = useState(false);
   const [showDropdownConcept, setShowDropdownConcept] = useState(false);
   const [savedResponse, setSavedResponse] = useState(null);
-  const [aiSolution, setAiSolution ] = useState("hi")
-
+  const [aiSolution, setAiSolution ] = useState(null)
 
   const toggleDropdownLang = () => {
-    setShowDropdownLang(!showDropdownLang);
+    setShowDropdownLang(!showDropdownLang); // drop down menu to toggle languages
   };
 
   const toggleDropdownLevel = () => {
-    setShowDropdownLevel(!showDropdownLevel);
+    setShowDropdownLevel(!showDropdownLevel); //drop down menu to toggle difficulty level
   };
 
   const toggleDropdownConcept = () => {
-    setShowDropdownConcept(!showDropdownConcept);
+    setShowDropdownConcept(!showDropdownConcept); // drop down menu to toggle concept
   };
 
   const handleLanguageSelection = (language) => {
-    setSelectedLanguage(language === selectedLanguage ? null : language);
+    setSelectedLanguage(language === selectedLanguage ? null : language); //if language is selected change state of drop down menu
     toggleDropdownLang();
   };
 
   const handleLevelSelection = (level) => {
-    setSelectedLevel(level === selectedLevel ? null : level);
+    setSelectedLevel(level === selectedLevel ? null : level); // if diffculty level is selected change state of drop down menu
     toggleDropdownLevel();
   };
 
   const handleConceptSelection = (concept) => {
-    setSelectedConcept(concept === selectedConcept ? null : concept);
+    setSelectedConcept(concept === selectedConcept ? null : concept); // if concept level is selected change state of drop down menu
     toggleDropdownConcept();
   };
 
-  let choices = `${selectedLanguage} ${selectedLevel} ${selectedConcept}`
-
-  //send and recieve post request to openai after selecting all required fields
-  const handleSelectedChoices = async (selectedLanguage, selectedLevel, selectedConcept) =>{
+  const handleSelectedChoices = async (selectedLanguage, selectedLevel, selectedConcept) =>{   //send and recieve post request to openai after selecting all required fields
     if (selectedLanguage == null || selectedLevel == null || selectedConcept == null){
-  toast("You Must Select a Language, Level and Concept before starting!");
-  return; 
-}
-let question = `Give me a coding algorithm question. The coding language is ${selectedLanguage}, make the difficulty level ${selectedLevel}, and make the concept on ${selectedConcept}`
-const url = window.location.pathname + '/airesponse';
+    toast("You Must Select a Language, Level and Concept before starting!");
+    return; }
+    let question = `Give me a coding algorithm question. The coding language is ${selectedLanguage}, make the difficulty level ${selectedLevel}, and make the concept on ${selectedConcept}`
+    const url = window.location.pathname + '/airesponse';
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json',},
       body: JSON.stringify({ prompt: question}),
     });
     let responseData = await response.json();
     setAiResponse(responseData.data)
     setSavedResponse(responseData.data);
-   
-    return responseData;
+    return responseData; 
   }
 
-const sendResponse = async () =>{
+const sendResponse = async () =>{ //send message including user's code to check on how user did
   let aiResponse = savedResponse;
   if (aiResponse === null){
     toast("You Must Select a Language, Level and Concept before getting a response!");
-    return;
+    return; 
   }
   console.log(savedResponse)
   let userCode = `${code}`
-  let toAi = `How does my code look? The question was ${aiResponse}. and my code is ${userCode}. Do not tell me the complete answer but give me a hint on what I need to fix.`
+  let toAi = `How does my code look? The question was ${aiResponse}. and my code is ${userCode}. Analyze the code indept! Tell me if it is correct. If it is not correct give me a hint on hoq to fix it.`
   console.log(toAi);
   const url = window.location.pathname + '/airesponse';
     const response = await fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
-      },
+        'Content-Type': 'application/json',},
       body: JSON.stringify({ prompt: toAi}),
     });
     let responseData = await response.json();
     setAiResponse(responseData.data)
-}
+  }
 
-
-const getSolution = async () =>{
+const getSolution = async () =>{ //get solution code from ai if user could not figure it out
   console.log(aiResponse)
   if (aiResponse === "Generate Your Question By Pressing Get Question Above!"){
     toast("You Must Select a Language, Level and Concept and Generate A Question Before Recieving a Solution!");
     return;
   }
-
   let toAi = `Can you show me the solution code. The question was ${aiResponse}.`
   const url = window.location.pathname + '/airesponse';
     const response = await fetch(url, {
@@ -109,190 +99,85 @@ const getSolution = async () =>{
     console.log(responseData)
     const newCodeString = responseData.data.replace(/\s{3,}/g, '\n');
       console.log(newCodeString);   
-
-
     setAiSolution(responseData.data)
     updateCodeMirrorValue(newCodeString)
-
 }
-
   return (
     <>
     <nav style={{ backgroundColor: "#000", padding: "10px" }}>
       <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
         <div className="relative">
-          <button
-            className="language"
-            style={{
-              color: "#fff",
-              cursor: "pointer",
-              backgroundColor: "transparent",
-              border: "none",
-              outline: "none",
-              padding: "5px",
-            }}
-            onClick={toggleDropdownLang}
-          >
-            {selectedLanguage ? selectedLanguage : "Languages"} ▼
+          <button className="language" style={{color: "#fff",cursor: "pointer",backgroundColor: "transparent",border: "none",
+              outline: "none",padding: "5px",
+            }} onClick={toggleDropdownLang}>
+            {selectedLanguage ? selectedLanguage : "Languages"} ▼ 
           </button>
           {showDropdownLang && (
             <ul
-              style={{
-                position: "absolute",
-                backgroundColor: "#333",
-                color: "#fff",
-                padding: "10px",
-                borderRadius: "5px",
-                top: "100%",
-                left: "0",
-                zIndex: "9999",
-              }}
-            >
+              style={{ position: "absolute", backgroundColor: "#333", color: "#fff", padding: "10px", borderRadius: "5px",
+                top: "100%", left: "0",zIndex: "9999",}}>
               <li>
-                <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLanguageSelection("JavaScript")}
-                >
-                  JavaScript
-                </a>
+                <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLanguageSelection("JavaScript")}> JavaScript</a>
               </li>
               <li>
-                <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLanguageSelection("Python")}
-                >
-                  Python
-                </a>
+                <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLanguageSelection("Python")}>Python</a>
               </li>
               <li>
-                <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLanguageSelection("C++")}
-                >
-                  C++
-                </a>
+                <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLanguageSelection("C++")}> C++</a>
               </li>
-            
-            </ul>
-          )}
+            </ul>)}
         </div>
         <div className="relative">
-          <button
-            style={{
-              color: "#fff",
-              cursor: "pointer",
-              backgroundColor: "transparent",
-              border: "none",
-              outline: "none",
-              padding: "5px",
-            }}
-            onClick={toggleDropdownLevel}
-          >
-            {selectedLevel ? selectedLevel : "Difficulty Level"} ▼
+          <button style={{color: "#fff",cursor: "pointer", backgroundColor: "transparent", border: "none",
+          outline: "none", padding: "5px",}} onClick={toggleDropdownLevel}>
+          {selectedLevel ? selectedLevel : "Difficulty Level"} ▼
           </button>
           {showDropdownLevel && (
-            <ul
-              style={{
-                position: "absolute",
-                backgroundColor: "#333",
-                color: "#fff",
-                padding: "10px",
-                borderRadius: "5px",
-                top: "100%",
-                left: "0",
-                zIndex: "9999",
-              }}
-            >
+            <ul style={{ position: "absolute", backgroundColor: "#333", color: "#fff", padding: "10px", borderRadius: "5px",
+            top: "100%", left: "0", zIndex: "9999", }}>
               <li>
-                <a
-                  style={{ textDecoration : "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLevelSelection("Beginner")}
-                  >
-                  Beginner
-                  </a>
+                <a style={{ textDecoration : "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLevelSelection("Beginner")}>Beginner </a>
                   </li>
                   <li>
-                  <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLevelSelection("Intermediate")}
-                  >
-                  Intermediate
-                  </a>
+                  <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLevelSelection("Intermediate")}>Intermediate</a>
                   </li>
                   <li>
-                <a
-                  style={{ textDecoration : "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLevelSelection("Difficult")}
-                  >
-                  Difficult
-                  </a>
+                <a style={{ textDecoration : "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLevelSelection("Difficult")}>Difficult</a>
                   </li>
                   <li>
-                <a
-                  style={{ textDecoration : "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleLevelSelection("Very Difficult")}
-                  >
-                  Very Difficult
-                  </a>
+                <a style={{ textDecoration : "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleLevelSelection("Very Difficult")}>Very Difficult</a>
                   </li>
-                  </ul>
-                  )}
-                  </div>
+                  </ul>)} </div>
                   <div className="relative">
-                  <button
-                  style={{
-                  color: "#fff",
-                  cursor: "pointer",
-                  backgroundColor: "transparent",
-                  border: "none",
-                  outline: "none",
-                  padding: "5px",
-                  }}
-                  onClick={toggleDropdownConcept}
-                  >
+                  <button style={{color: "#fff",cursor: "pointer",backgroundColor: "transparent",border: "none",
+                  outline: "none",padding: "5px",}}onClick={toggleDropdownConcept}>
                   {selectedConcept ? selectedConcept : "Concept"} ▼
                   </button>
                   {showDropdownConcept && (
-                  <ul
-                  style={{
-                  position: "absolute",
-                  backgroundColor: "#333",
-                  color: "#fff",
-                  padding: "10px",
-                  borderRadius: "5px",
-                  top: "100%",
-                  left: "0",
-                  zIndex: "9999",
-                  }}
-                  >
+                  <ul style={{position: "absolute",backgroundColor: "#333",color: "#fff",padding: "10px",borderRadius: "5px",
+                  top: "100%",left: "0", zIndex: "9999",}}>
                   <li>
-                  <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleConceptSelection("Functions")}
-                  >
-                  Functions
-                  </a>
+                  <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleConceptSelection("Functions")}>Functions</a>
                   </li>
                   <li>
-                  <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleConceptSelection("Arrays")}
-                  >
-                  Arrays
-                  </a>
+                  <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleConceptSelection("Arrays")}>Arrays</a>
                   </li>
                   <li>
-                  <a
-                  style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
-                  onClick={() => handleConceptSelection("OOP (Object Oriented Programming)")}
-                  >
+                  <a style={{ textDecoration: "none", color: "#fff", zIndex: "9999", cursor: "pointer" }}
+                  onClick={() => handleConceptSelection("OOP (Object Oriented Programming)")}>
                   OOP (Object Oriented Programming)
                   </a>
-                  </li>
-                  </ul>
-                  )}
-                  </div>
-                  </div>
+                  </li> </ul>)}
+                  </div> </div>
                   <div style={{ marginTop: "10px", color: "#fff" }}>
                   Selected Choices: {selectedLanguage} {selectedLevel} {selectedConcept}
                   <br />
@@ -300,8 +185,7 @@ const getSolution = async () =>{
            color: '#ffffff', borderRadius: '3px', cursor: 'pointer'}} onClick={() => handleSelectedChoices(selectedLanguage, selectedLevel, selectedConcept)}>
                   Get Question
                     </button>
-              
-                  </div>
+               </div>
                   </nav>
                   <div style={{ fontFamily: 'Arial, sans-serif', backgroundColor: '#f5f5f5', margin: 0, padding: '20px' }}>
       <div style={{ maxWidth: '800px', margin: '0 auto', backgroundColor: '#ffffff', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)', borderRadius: '5px', overflow: 'hidden' }}>
@@ -315,10 +199,8 @@ const getSolution = async () =>{
               {aiResponse}
               </div>
           </div>
-          {}
         </div>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '10px', backgroundColor: '#f0f0f0' }}>
-         
           <button style={{ marginLeft: '10px', padding: '8px 15px', border: 'none', backgroundColor: '#4caf50',
            color: '#ffffff', borderRadius: '3px', cursor: 'pointer'}} onClick={()=> sendResponse()}> Submit Your Code</button>
            <button style={{ marginLeft: '10px', padding: '8px 15px', border: 'none', backgroundColor: '#4caf50',
@@ -327,22 +209,6 @@ const getSolution = async () =>{
       </div>
     </div>
     <ToastContainer />
-        </>
-                  
-                 ); };
-                  
-                  export default NavBar;
-
-               
-
-
-               
-
-
-               
-
-               
-
-               
-
+        </>  ); };
+       export default NavBar;
 
