27796e6cca68d5b7fb40a476de66d315d8b63ae5
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-20 21:51:52 -0700

    Update resolvers.js
    update to include saved work resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,9 +5,9 @@ const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
-    // Query: {
+    Query: {
 
-    // },
+    },
     Mutation: {
         async login(parent, { email, password }) {
             const user = await User.findOne({ email });
@@ -22,15 +22,31 @@ const resolvers = {
             const token = signToken(user);
             return { token, user }
         },
-        async addUser(parent, { email, password }) {
+        async addUser(parent, {email, password, username}) { 
             const user = await User.create({
+                username: username,
                 email: email,
                 password: password
             });
             const token = signToken(user);
             return { token, user }
-        }
+        },
+        async saveWork(parent,{solutionData}, context) {
+            if(context){       
+        
+            let filter = {_id: context.user._id }  
+          
+                const user = await User.findOneAndUpdate(filter,
+                    { $push: { savedWork : solutionData}}, 
+                    {new: true});
+                    return user;
+
+            } else {
+                throw new AuthenticationError('You need to be logged in!');
+              }
+
+        } 
 
     }
 }
-module.exports = resolvers
+module.exports = resolvers;
